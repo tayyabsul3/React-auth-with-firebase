44e96774ef26afffe1b72b8c9481704e10341d72
Muhammad Tayyab <tayyabsultan621@gmail.com>
2024-07-16 14:38:35 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import Login from "./components/Login";
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Login />
-    </>
+    </React.Fragment>
   );
 }
 
